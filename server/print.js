c2c0fd2f2847f08fd4b3567b946f354707fdc124
Sergey Barsukov <barsukof@live.com>
2017-09-03 16:55:12 +0300

    Frontend лекция 1

---
+++ server/print.js
@@ -0,0 +1,12 @@
+function printHello() {
+    console.log('Hello');
+}
+
+function printGoodbye() {
+    console.log('Tschüss');
+}
+
+module.exports = {
+    printHello : printHello,
+    printGoodbye: printGoodbye
+};
